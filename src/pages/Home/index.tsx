b2d4b2307c8a7d0dfec6d778c0d707d64e3b2709
Cauê Almeida <nncl@live.com>
2023-03-08 23:13:01 -0300

    feat(pages): Handles enter

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
-import {FlatList, Image, SafeAreaView, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {Alert, FlatList, Image, SafeAreaView, Text, TextInput, TouchableOpacity, View} from 'react-native';
 
 import logo from '../../../assets/logo.png';
-import add from '../../../assets/plus.png';
+import addIcon from '../../../assets/plus.png';
 import clipboard from '../../../assets/clipboard.png';
 import trash from '../../../assets/trash.png';
 
@@ -9,10 +9,25 @@ import {styles} from "./styles";
 import {useState} from "react";
 import {Checkbox} from "expo-checkbox";
 
+interface Task {
+    value: string;
+    active: true,
+}
+
 export function Home() {
     const [focus, setFocus] = useState(false);
-    const [isChecked, setChecked] = useState(false);
-    const [items, setItems] = useState([1,2,3]);
+
+    const [task, setTask] = useState('');
+    const [items, setItems] = useState<Task[]>([]);
+
+    const add = () => {
+        if (!task) {
+            return Alert.alert('Ops', 'You forgot to type!')
+        }
+
+        setItems(prevState => [...prevState, {value: task, active: true}]);
+        setTask('');
+    }
 
     return (
         <SafeAreaView style={styles.container}>
@@ -23,9 +38,13 @@ export function Home() {
                     <TextInput style={[styles.input, focus && styles.inputFocus]}
                                onFocus={() => setFocus(true)}
                                onBlur={() => setFocus(false)}
-                               placeholder='Adicione uma nova tarefa' placeholderTextColor='#808080'/>
-                    <TouchableOpacity style={styles.button}>
-                        <Image source={add}/>
+                               placeholder='Adicione uma nova tarefa' placeholderTextColor='#808080'
+                               onChangeText={setTask}
+                               value={task}
+                               enablesReturnKeyAutomatically={true}
+                               onSubmitEditing={add}/>
+                    <TouchableOpacity style={styles.button} onPress={add}>
+                        <Image source={addIcon}/>
                     </TouchableOpacity>
                 </View>
 
@@ -34,7 +53,7 @@ export function Home() {
                         <Text style={[styles.headerText, styles.colorBlue]}>Criadas</Text>
 
                         <View style={styles.count}>
-                            <Text style={styles.countText}>1</Text>
+                            <Text style={styles.countText}>{items.length}</Text>
                         </View>
                     </View>
 
@@ -50,22 +69,23 @@ export function Home() {
                           showsVerticalScrollIndicator={false}
                           style={styles.list}
                           renderItem={({item}) => (
-                              <View style={[styles.item, item === 2 && styles.itemSelected]} key={item}>
-                                  <Checkbox style={[styles.itemCheckbox, isChecked && styles.itemCheckboxActive]}
-                                            color={isChecked ? '#5E60CE' : '#4EA8DE'}
-                                            value={isChecked} onValueChange={setChecked}/>
-                                  <Text style={[styles.itemText, item === 2 && styles.itemTextSelected]} numberOfLines={2}>
-                                      Integer urna interdum massa libero auctor neque turpis turpis semper.
+                              <View style={[styles.item, item.active && styles.itemSelected]} key={item}>
+                                  <Checkbox style={[styles.itemCheckbox, !item.active && styles.itemCheckboxActive]}
+                                            color={!item.active ? '#5E60CE' : '#4EA8DE'}
+                                            value={!item.active}/>
+                                  <Text style={[styles.itemText, !item.active && styles.itemTextSelected]}
+                                        numberOfLines={2}>
+                                      {item.value}
                                   </Text>
 
                                   <TouchableOpacity style={styles.itemButton}>
-                                      <Image source={trash} />
+                                      <Image source={trash}/>
                                   </TouchableOpacity>
                               </View>
                           )}
                           ListEmptyComponent={() => (
                               <View style={styles.empty}>
-                                  <Image style={styles.emptyLogo} source={clipboard} />
+                                  <Image style={styles.emptyLogo} source={clipboard}/>
 
                                   <Text style={[styles.emptyText, styles.emptyTextBold]}>
                                       Você ainda não tem tarefas cadastradas
