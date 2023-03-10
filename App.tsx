2cfb3a3a6a331a41c18275902927cb802d395e76
Cauê Almeida <nncl@live.com>
2023-03-09 22:23:49 -0300

    style: Formats App.tsx

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
-import {Home} from "./src/pages/Home";
-import {StatusBar} from "expo-status-bar";
-import {useFonts, Inter_400Regular, Inter_700Bold} from "@expo-google-fonts/inter";
+import { Home } from './src/pages/Home';
+import { StatusBar } from 'expo-status-bar';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_700Bold,
+} from '@expo-google-fonts/inter';
 
 export default function App() {
-    let [fontsLoaded] = useFonts({
-        Inter_400Regular,
-        Inter_700Bold
-    });
+  let [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_700Bold,
+  });
 
-    if (!fontsLoaded) {
-        return null;
-    }
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
     <>
